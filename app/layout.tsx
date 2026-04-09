3186810ef4d6c7754725fcdd69a4fd3c7f8c8cf7
NichitaCon <nichitacon04@gmail.com>
2026-04-09 15:10:52 +0200

    Vercel statistics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 import SiteFooterSection from "./components/home/SiteFooterSection";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
