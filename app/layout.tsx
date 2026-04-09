88da1f6d5c4d23ba300a0fa28b8cc1d52d6a00f7
NichitaCon <nichitacon04@gmail.com>
2026-04-09 18:31:32 +0200

    Fix Vercel Analytics integration in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased`}
       >
         {children}
+        <Analytics />
         {/* <SiteFooterSection/> */}
       </body>
     </html>
